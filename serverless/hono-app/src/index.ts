cd21c2108c02c87027fcc376c5c075e785e6b286
vinay satrasala <satrasalavinaykumar.01@gmail.com>
2024-10-17 00:25:00 +0530

    from personal

--- serverless/hono-app/src/index.ts
+++ serverless/hono-app/src/index.ts
@@ -1,14 +1,25 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
+// Writing middlewares in hono
+async function authMiddleware(c:any,next:any){
+  if(c.req.header("Authorization")){
+    await next();
+  }else{
+    return c.text("Not authenticated......!");
+  }
+}
+// app.use(authMiddleware);
+app.get('/',authMiddleware,async (c) => {
+  // const body = await c.req.json();
 
-app.post('/',async (c) => {
-  const body = await c.req.json();
-
-  console.log(body);
-  console.log(c.req.header);
-  console.log(c.req.query);
+  // console.log(body);
+  // console.log(c.req.header);
+  // console.log(c.req.query);
   return c.text('Hello Hono!')
+});
+app.get("/hello",async (c)=>{
+  return c.text("From hello.......!");
 })
 
 export default app
